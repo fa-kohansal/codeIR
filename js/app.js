170f8cc79cb3a65b07fc742b41b8c0774d7ac4a1
fa-kohansal <farnaz_kohansal@yahoo.com>
2023-09-19 15:26:20 +0330

    complete side bar

---
+++ js/app.js
@@ -0,0 +1,7 @@
+$('#home').click(()=>{
+    $('#ourProject').addClass('d-none')
+})
+$('#projectBtn').click(()=>{
+    $('#homePage').addClass('d-none');
+    $('#ourProject').removeClass('d-none')
+})
