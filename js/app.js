9b27d4a492e98cf2d2b0fb33323153fe1e5f64cc
fa-kohansal <farnaz_kohansal@yahoo.com>
2023-09-20 15:14:37 +0330

    jquery set timeout homePage

--- js/app.js
+++ js/app.js
@@ -3,6 +3,10 @@ setTimeout(function (){
     $('.wrapper').addClass('d-flex')
     $('.wrapper').removeClass('d-none')
 },3000)
+setTimeout(function (){
+    $('.sidebar').fadeIn(800).addClass('animate__animated animate__fadeInLeft')
+    $('.content').fadeIn(800).addClass('animate__animated animate__fadeInRight')
+},3000)
 $('#home').click(()=>{
     $('#ourProject').addClass('d-none')
 })
